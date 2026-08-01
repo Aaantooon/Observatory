f4a19cf0aad2ea2e46d38289462e07a2af2d5b3e
Anton_Gromakov <hamprer2015@gmail.com>
2026-08-01 20:24:07 +0300

    Исправлен settings.py: добавлен DIRS для шаблонов

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',          # твоё приложение
+    'core',
 ]
 
 # --- МИДЛВАРЫ ---
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],   # ← ДОБАВЛЕНО!
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
