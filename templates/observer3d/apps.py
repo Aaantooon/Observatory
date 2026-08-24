d04f3efa0ea4b6ed6c14a4901d4c7d77430bf4ce
Anton_Gromakov <hamprer2015@gmail.com>
2026-08-25 00:31:29 +0300

    chore: cleanup unused files, add 3D game

--- templates/observer3d/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class Observer3dConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'observer3d'
-    verbose_name = '3D Мир Наблюдателя'
--- templates/observer3d/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def babylon_world(request):
-    return render(request, 'observer3d/world_babylon.html')
