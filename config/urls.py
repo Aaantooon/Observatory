48b72355e85161de40691fb1d1a53061e9ac27f8
Anton_Gromakov <hamprer2015@gmail.com>
2026-08-28 21:23:33 +0300

    fix: restore vk_login_start route

--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from .admin import admin_site  # Импортируем кастомную админку
+from myapp.vk_id_auth import vk_login_start, vk_login_callback
 
 urlpatterns = [
     path('admin/', admin_site.urls),  # Используем кастомную админку
     path('api/', include('bot_api.urls')),
     #path('forum/', include('machina.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('vk/login/', vk_login_start, name='vk_login_start'),
+    path('vk/callback/', vk_login_callback, name='vk_login_callback'),
     
     # VK OAuth через social_django
     path('', include('social_django.urls', namespace='social')),
