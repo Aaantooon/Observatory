9f37742ad869627b17d3ed1d657ee6fe82200fe9
Anton_Gromakov <hamprer2015@gmail.com>
2026-08-30 22:37:10 +0300

    Добавлены модули курса в админку

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Module, UserCourseProgress, ModuleComment
 
-# Register your models here.
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('number', 'title', 'is_published', 'order', 'duration')
+    list_editable = ('is_published', 'order')
+    search_fields = ('title', 'subtitle', 'description')
+    ordering = ('number',)
+
+
+@admin.register(UserCourseProgress)
+class UserCourseProgressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'current_module', 'started_at', 'completed_at')
+    search_fields = ('user__username',)
+
+
+@admin.register(ModuleComment)
+class ModuleCommentAdmin(admin.ModelAdmin):
+    list_display = ('module', 'user', 'created_at')
+    search_fields = ('text', 'user__username')
